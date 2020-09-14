eb8c5cf1aefa8086742ee61eb9197d6d3753ecbc
juantrinidad14 <70726489+juantrinidad14@users.noreply.github.com>
2020-09-13 22:30:12 -0400

    Add files via upload

---
+++ TodoApp.py
@@ -0,0 +1,41 @@
+
+press_1 = ("Pres 1 to add taks\n")
+press_2 = ("Press 2 to delete task\n")
+press_3 = ("Press 3 to view all tasks\n")
+press_q = ("press q to quit\n")
+
+print(press_1, press_2, press_3, press_q)
+
+def answer (press_q):
+    print("All information will be deleted.")
+
+answer("q")
+
+title_1 = input(:"Enter a title: ")
+priority_0 = input("Priority (high, medium,and low: ")
+
+wash = "1. Wash the car - high"
+mow = "2. Mow the lawn - low"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
